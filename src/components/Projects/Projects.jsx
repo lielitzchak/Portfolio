59dcc1229bacb1cb3bd4d3a7c01f89f5985757a1
liel itzchak <liel.izcha321@gmail.com>
2022-02-19 18:09:37 +0200

    Component design

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,12 +1,13 @@
 import { MY_PROJECTS } from "./Project";
+import "./Project.css";
 export default function Projects() {
   return (
     <article className="Projects">
       {MY_PROJECTS.map((project, i) => {
         return (
-          <section key={i}>
-            <img src={project.photo} alt={project.name} />
+          <section className="DetailsProject" key={i}>
             <h1>{project.name}</h1>
+            <img src={project.photo} alt={project.name} />
             <p>{project.description}</p>
             <a href={project.link}>link to Github</a>
           </section>

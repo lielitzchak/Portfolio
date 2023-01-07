1a8bca79e9527f8aed03d4121899cbadd2ab0e12
liel itzchak <liel.izcha321@gmail.com>
2023-01-07 22:51:28 +0200

    add scss

--- src/components/About/About.component.js
+++ src/components/About/About.component.js
@@ -1,13 +1,41 @@
+import AboutBottomPicturersComponent from "./AboutBottomPicturersComponent";
+
+ 
 export default function About() {
   return (
     <div className="About information">
-      I’m a Front-End Developer located in Poland. I have a serious passion for
-      UI effects, animations and creating intuitive, dynamic user experiences.
-      Well-organised person, problem solver, independent employee with high
-      attention to detail. Fan of MMA, outdoor activities, TV series and English
-      literature. A family person and father of two fractious boys, Interested
-      in the entire frontend spectrum and working on ambitious projects with
-      positive people.
+      <div className="AboutUp">
+        <section className="AboutMe">
+          <h1> I am a full-stack developer.</h1>
+          <p>
+            Some of my passions include problem solving, coding, and thinking
+            outside the box.
+          </p>
+          <p>
+            I am a well-organized, independent employee (also an excellent team
+            player) with a high level of attention to detail. A fan of
+            Powerlifting, TV series, anime, and books that are related to
+            personal development, career advancement, and interpersonal
+            relationships.
+          </p>
+          <p>
+            Whenever I'm not coding, you can find me at the gym, learning
+            Amharic, or looking for interesting videos on TED.
+          </p>
+        </section>
+        <div className="pictureOfMe">
+          <img
+            src="https://media-exp1.licdn.com/dms/image/C4D03AQHc4WsCx9L49g/profile-displayphoto-shrink_800_800/0/1642848341301?e=1675900800&v=beta&t=zsPVh-6P9zqLuRgJuiv3_RvaN1DE8HGq6TjKr4ZkMgE"
+            alt="my profile on linkedin"
+          />
+        </div>
+      </div>
+       <div className="AboutBottom">
+        <div className="line"></div>
+        <section className="picturers">
+       <AboutBottomPicturersComponent/>
+        </section>
+      </div>
     </div>
   );
 }
--- src/components/Home/Home.component.jsx
+++ src/components/Home/Home.component.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 export default function Home() {
+  let space = ` `;
   return (
     <div className="information Home">
       <span>&lt;html&gt;</span>
       <span>&lt;body&gt;</span>
-      <span> &lt;h1&gt;</span>
-      <h1> Hi, Im Liel full stack development!</h1>
-      <span> &lt;/h1&gt;</span>
-      <span> &lt;p&gt;</span>
+      <span>&lt;h1&gt;</span>
+      <h1>Hi, Im Liel full stack development!</h1>
+      <span>&lt;/h1&gt;</span>
+      <span>&lt;p&gt;</span>
       <p>frontend, backend and full stack</p>
       <span> &lt;/p&gt;</span>
       <button>
